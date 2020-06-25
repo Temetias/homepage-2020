e874543d870a4d9ce52b665c6f049d50c202fa23
Temetias <karppinenteemu@outlook.com>
2020-06-25 13:24:24 +0300

    Tweak autodeploy

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,10 @@ echo "Deploying to Temetias.github.io..."
 git clone https://github.com/Temetias/Temetias.github.io.git tmp
 npm run generate
 mkdir tmp2
-mv tmp/* tmp2/
-mv __sapper__/export/* tmp/
-mv tmp2/* tmp/ 2>/dev/null
-cd tmp/
+mv tmp/.git/ tmp2/
+mv tmp/CNAME tmp2/
+mv __sapper__/export/* tmp2/
+cd tmp2/
 git add .
 git commit -m "autodeploy"
 git push && cd .. && rm -rf tmp/ tmp2/
