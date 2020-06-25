58180070f4777bd2af7cf7809c425c209916ea38
Temetias <karppinenteemu@outlook.com>
2020-06-25 11:55:45 +0300

    Extract blog post header to own component

--- src/components/BlogPostHeader.svelte
+++ src/components/BlogPostHeader.svelte
@@ -1,3 +1,64 @@
 <script>
+  export let post;
 
+  const DEFAULT_AUTHOR = "Teemu Karppinen";
+  const DEFAULT_AUTHOR_IMG = "/me2.jpg";
+
+  function idFy(str) {
+    return str
+      .toLowerCase()
+      .split(" ")
+      .join("-");
+  }
+
+  function getAuthor(post) {
+    return post.author || DEFAULT_AUTHOR;
+  }
+
+  function getAuthorImg(post) {
+    return post.authorImg || DEFAULT_AUTHOR_IMG;
+  }
 </script>
+
+<style>
+  header {
+    margin-bottom: 2em;
+  }
+
+  h1 {
+    margin-bottom: 1em;
+  }
+
+  .post-info {
+    display: flex;
+  }
+
+  .post-info img {
+    margin: 0;
+    border: 1px solid white;
+    border-radius: 50%;
+    width: 4em;
+    height: 4em;
+  }
+
+  .post-info-inner {
+    margin-left: 1em;
+  }
+
+  .post-info-inner span {
+    display: block;
+  }
+</style>
+
+<header>
+  <h1 id={idFy(post.title)}>{post.title}</h1>
+  <div class="post-info">
+    <img src={getAuthorImg(post)} alt={getAuthor(post)} />
+    <div class="post-info-inner">
+      <span>{getAuthor(post)}</span>
+      <span>
+        {new Date(post.date).toLocaleDateString()} • {post.readTime} min read
+      </span>
+    </div>
+  </div>
+</header>
